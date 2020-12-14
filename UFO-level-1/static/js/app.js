ec7d42e9687fca083bcbdfd4e25f976008d8ac16
James Williams <jameswilliams@Jamess-MBP.lan>
2020-12-14 15:21:40 -0500

    12/14/2020 15:21

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,42 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+// Data Table Creation Function
+function tableCreation(data){
+    data.forEach((alienReport) => {
+    var row = tbody.append("tr");
+    Object.entries(alienReport).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
+}
+
+tableCreation(tableData);
+
+// Configure Date Filter
+var button = d3.select("#filter-btn");
+
+var form = d3.select("#filters");
+
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Create the function to run for events
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+  
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    var filteredData = people.filter(instance => instance.datetime === inputValue);
+
+    tableCreation(filteredData);
+  }
