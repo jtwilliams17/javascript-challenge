deae2a930700f86917c0d1b362245ec9889f8fff
James Williams <jameswilliams@Jamess-MacBook-Pro.local>
2020-12-26 19:40:50 -0500

    level 1 complete

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,39 +5,35 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // Data Table Creation Function
-function tableCreation(data){
-    data.forEach((alienReport) => {
+function tableCreation(data) {
+  tbody.html("");
+
+  data.forEach((dataRow) => {
+
     var row = tbody.append("tr");
-    Object.entries(alienReport).forEach(([key, value]) => {
+
+    Object.values(dataRow).forEach((value) => {
       var cell = row.append("td");
-      cell.text(value);
-    });
+        cell.text(value);
+      }
+    );
   });
 }
-
-tableCreation(tableData);
-
-// Configure Date Filter
-var button = d3.select("#filter-btn");
-
-var form = d3.select("#filters");
-
-button.on("click", runEnter);
-form.on("submit",runEnter);
-
 // Create the function to run for events
 function runEnter() {
-
-    // Prevent the page from refreshing
-    d3.event.preventDefault();
   
     // Select the input element and get the raw HTML node
-    var inputElement = d3.select("#datetime");
+    var date = d3.select("#datetime").property("value");
   
-    // Get the value property of the input element
-    var inputValue = inputElement.property("value");
-  
-    var filteredData = people.filter(instance => instance.datetime === inputValue);
+    var filteredData = tableData;
+
+    if (date) {
+
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
 
     tableCreation(filteredData);
   }
+  d3.selectAll("#filter-btn").on("click", runEnter);
+
+  tableCreation(tableData);
